77e2e1f796c5b200e56cee38d8d07c96a55a2999
rikaz81 <rikaz81@gmail.com>
2017-06-01 06:07:02 +0530

    filter pipe

--- my-demo-app/src/app/shared/pipes/filter.pipe.ts
+++ my-demo-app/src/app/shared/pipes/filter.pipe.ts
@@ -5,10 +5,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterPipe implements PipeTransform {
 
+    filteredItems: any = [];
     transform(items: any[], filter: string): any {
         console.log('inside text filter');
         console.log('Items', items);
         console.log('Filters', filter);
+
         if (!items || !filter) {
             return items;
         }
@@ -16,8 +18,16 @@ export class FilterPipe implements PipeTransform {
 
 
 
-        items = items.filter(item => item.title.toLowerCase().indexOf(filter.toLowerCase()) !== -1);
+        // items = items.filter(item => item.title.toLowerCase().indexOf(filter.toLowerCase()) !== -1);
+
+        items = items.filter(item => item.brand.toLowerCase().indexOf(filter.toLowerCase()) !== -1);
         console.log(items.length);
+
+        if (items.length > 0)
+            items = items.filter(item => item.vechicleModel.toLowerCase().indexOf(filter.toLowerCase()) !== -1);
+
+        console.log(items.length);
+
         return items;
 
         // To search in values of every variable of your object(item)
